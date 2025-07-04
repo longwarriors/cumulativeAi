fda3f5606d8f87b3b27a14fe4ed6ffece752a75f
lenovo2024 <sharpwave@hotmail.com>
2025-07-04 19:04:13 +0800

    伴随trainer更新

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,8 @@
 from .preprocess_images import trim, fast_resize_images
 from .plot_chinese_font import check_available_cn_fonts, setup_chinese_font
-from .trainer import train_epoch, validate_epoch
+from .trainer import (save_checkpoint,
+                      load_checkpoint,
+                      EarlyStopping,
+                      train_epoch,
+                      validate_epoch,
+                      train_loop_with_resume, )
